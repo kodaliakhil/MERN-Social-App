cdcac4493ef132ca6cac4ce4de14411379a7a2b0
kodaliakhil <kodaliakhil1999@gmail.com>
2024-04-10 16:10:03 +0530

    fetch changes

--- frontend/src/components/LoginCard.jsx
+++ frontend/src/components/LoginCard.jsx
@@ -33,7 +33,7 @@ export default function LoginCard() {
   const handleLogin = async () => {
     setLoading(true);
     try {
-      const res = await fetch("/api/users/login", {
+      const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/users/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,14 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // to get rid of cors error
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: false,
-        secure: false,
-      },
-    },
-  },
+  // server: {
+  //   // to get rid of cors error
+  //   proxy: {
+  //     "/api": {
+  //       target: "http://localhost:3000",
+  //       changeOrigin: false,
+  //       secure: false,
+  //     },
+  //   },
+  // },
 });
