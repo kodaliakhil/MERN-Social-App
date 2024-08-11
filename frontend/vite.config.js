4c791aea31b256ffa0630f4a9da2b40c27293e54
kodaliakhil <kodaliakhil1999@gmail.com>
2024-08-11 21:12:15 +0530

    docker setup

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host:true
+  }
   // server: {
   //   // to get rid of cors error
   //   proxy: {
